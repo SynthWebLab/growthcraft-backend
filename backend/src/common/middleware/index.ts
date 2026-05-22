663df1b414452389ce5dddd65c6e0ad3ee651e17
sandipan goswami <sandipangoswami205@gmail.com>
2026-05-22 10:06:59 +0530

    comment out the rate limiter for now

--- backend/src/common/middleware/index.ts
+++ backend/src/common/middleware/index.ts
@@ -6,7 +6,8 @@ export {
 export { asyncHandler } from '../utils/async-handler.util';
 export { authenticate, optionalAuthenticate } from './authenticate.middleware';
 export { authorize } from './authorize.middleware';
-export { apiLimiter, authLimiter, passwordResetLimiter } from './rate-limiter.middleware';
+// export { apiLimiter } from './rate-limiter.middleware';
+export { authLimiter, passwordResetLimiter } from './rate-limiter.middleware';
 // Note: The following middleware are not yet implemented
 // export { validate } from './validate.middleware';
 // export { sanitizeInput } from './sanitize.middleware';
